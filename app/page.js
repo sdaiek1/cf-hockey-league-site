64d8312e385b573f2857f23fd6940e9ee71c64ea
sdaiek1 <shane.daiek@gmail.com>
2026-03-25 18:33:47 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -387,6 +387,7 @@ export default async function HomePage() {
                     padding: "16px 18px",
                     borderRadius: 14,
                     fontWeight: 800,
+                    marginTop: 4,
                     boxShadow: "0 0 16px rgba(34,211,238,0.18)",
                   }}
                 >
