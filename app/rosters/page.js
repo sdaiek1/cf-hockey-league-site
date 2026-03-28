9d24a4c002ccecc2fbc026efd47ecb8367d86b79
sdaiek1 <shane.daiek@gmail.com>
2026-03-27 22:34:32 -0400

    Update page.js

--- app/rosters/page.js
+++ app/rosters/page.js
@@ -218,7 +218,7 @@ export default async function RosterPage({ searchParams }) {
               </select>
 
               <button type="submit" style={submitButton}>
-                Update
+                Go
               </button>
             </form>
           )}
