bf47aa1cdab38b1708acddd55bed496419def10a
sdaiek1 <shane.daiek@gmail.com>
2026-04-22 12:34:45 -0400

    Update page.js

--- app/schedule/page.js
+++ app/schedule/page.js
@@ -397,8 +397,8 @@ export default async function SchedulePage({ searchParams }) {
                           src={getTeamLogoSrc(game.away_team?.name)}
                           alt={`${game.away_team?.name || "Away team"} logo`}
                           style={{
-                            width: 75,
-                            height: 75,
+                            width: 100,
+                            height: 100,
                             objectFit: "contain",
                           }}
                         />
