17ace0da94b815c6f2ddefd13143c49ae5e0f5f9
sdaiek1 <shane.daiek@gmail.com>
2026-04-22 11:48:09 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,21 @@ export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 import { createClient } from "@supabase/supabase-js";
-import { getTeamLogoSrc } from "@/lib/teamLogos";
+
+function getTeamLogoSrc(teamName = "") {
+  const TEAM_LOGOS = {
+    "Team Rasta": "/Rasta_Logo.JPG",
+    "Zero Pucks Given": "/ZPG_Logo.PNG",
+    "Mayhem": "/Mayhem_Logo.png",
+    "Swiss Army": "/Swiss_Logo.PNG",
+    "WCFD": "/WCFD_Logo.PNG",
+    "H-Town Assassins": "/logo.png",
+    "Replacements": "/logo.png",
+    "Venom": "/Venom_Logo.JPG",
+  };
+
+  return TEAM_LOGOS[teamName] || "/logo.png";
+}
 
 export default async function HomePage() {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
