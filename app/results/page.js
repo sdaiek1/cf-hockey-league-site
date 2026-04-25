bfcc3e51534175b2b2da20b746c9f4c1f1274735
sdaiek1 <shane.daiek@gmail.com>
2026-04-25 09:05:49 -0400

    Update page.js

--- app/results/page.js
+++ app/results/page.js
@@ -10,8 +10,8 @@ function getTeamLogoSrc(teamName = "") {
     "Mayhem": "/Mayhem_Logo.png",
     "Swiss Army": "/Swiss_Logo.PNG",
     "WCFD": "/WCFD_Logo.PNG",
-    "H-Town Assassins": "/logo.png",
-    "Replacements": "/logo.png",
+    "H-Town Assassins": "/H-Town_Logo.png",
+    "Replacements": "/Replacements_Logo.png",
     "Venom": "/Venom_Logo.JPG",
   };
 
