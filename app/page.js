e905066573adb4a7e9365326a8704033f56e931b
sdaiek1 <shane.daiek@gmail.com>
2026-08-02 09:18:11 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -348,7 +348,7 @@ export default async function HomePage() {
   const heroSlideHoldPct = heroSlideVisiblePct * 0.82;
   const heroSlideFadeOutPct = heroSlideVisiblePct;
 
-  const announcementScrollSeconds = 25;
+  const announcementScrollSeconds = 11;
   const announcementStartEverySeconds = 7;
   const announcementCount = announcements.length;
   const announcementLoopSeconds =
